525f892ab583747d10509c08022924166249ad4f
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-06 23:06:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,15 @@ end
 def using_concat (array,arr2)
   array.concat(arr2)
 end
+
+def using_insert (array,element)
+  array.insert(4,element)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.shift
+end
